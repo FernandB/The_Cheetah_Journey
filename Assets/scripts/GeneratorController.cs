cdcde857dd9280b32594dc22b069e7460024228b
FernandB5 <battisti.fernand@gmail.com>
2018-05-30 12:53:04 +0200

    version 2, completely modified

---
+++ Assets/scripts/GeneratorController.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorController : MonoBehaviour {
+
+    [SerializeField]
+    int minPlatformSize = 1;
+    [SerializeField]
+    int maxPlatformSize = 10;
+    [SerializeField]
+    int maxHazardSize = 3;
+    [SerializeField]
+    int maxHeight = 3;
+    [SerializeField]
+    int maxDrop = -3;
+  
+   
+
+    [SerializeField]
+    [Range(0.0f,1.0f)]
+    float hazardChance = 0.5f;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    float spikesChance = 0.5f;
+
+    [SerializeField]
+    GameObject floorTop;
+    [SerializeField]
+    GameObject floorBottom;
+    [SerializeField]
+    GameObject spikes;
+
+    private int blockHeight = 0;
+
+    private float blockNum = 0;
+    private bool isHazard = false;
+    private float posXArrival= 121f;
+    private const int YMAX= 19;
+    private const int YMIN= -21;
+    private const float yDelta = 0.8f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Calculate the maximum number of platform on x axis
+       int maxNbPlatformsHorizontal = Mathf.RoundToInt(posXArrival / floorTop.GetComponent<Renderer>().bounds.size.x);
+        
+       
+        for (int i = 0; i < maxNbPlatformsHorizontal; i++)
+        {
+
+          
+            Random.InitState(System.DateTime.Now.Millisecond);
+
+            blockHeight = blockHeight + Mathf.RoundToInt(Random.Range(maxDrop, maxHeight));
+           
+            if (blockHeight > YMAX)
+                blockHeight = YMAX;
+
+            if (blockHeight < YMIN)
+                blockHeight = YMIN;
+
+            PopSpikes();
+            Random.InitState(System.DateTime.Now.Millisecond);
+
+            int platformSize = Mathf.RoundToInt(Random.Range(minPlatformSize, maxPlatformSize));
+           
+                Instantiate(floorTop, new Vector2(blockNum, blockHeight), Quaternion.identity);
+
+                for (int x = 1; x < platformSize; x++)
+                {
+                    Instantiate(floorBottom, new Vector2(blockNum, blockHeight - x), Quaternion.identity);
+                }
+
+                blockNum+=floorTop.GetComponent<Renderer>().bounds.size.x;
+
+
+
+           
+
+        }
+            
+
+
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+   void PopSpikes()
+    {
+        
+        Random.InitState(System.DateTime.Now.Millisecond);
+
+        if (isHazard)
+        {
+            isHazard = false;
+        }
+        else
+        {
+            if (Random.value < hazardChance)
+                isHazard = true;
+            else
+                isHazard = false;
+        }
+
+        if (isHazard)
+        {
+            Random.InitState(System.DateTime.Now.Millisecond);
+            if (Random.value < spikesChance)
+            {
+
+                //Generate spikes
+                
+              Instantiate(spikes, new Vector2(blockNum, blockHeight+yDelta), Quaternion.identity);
+               
+                
+            }
+            
+        }
+    }
+
+ 
+}
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,193 +1,85 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
-    [SerializeField]
-    private Transform maxpos;
 
     [SerializeField]
-    private Transform minpos;
+    float speedMove = 5f;
     [SerializeField]
-    private Transform hillPos;
-    [SerializeField]
-    private Transform treePos;
-    private static float hillBottom;
-   [SerializeField]
-    private  Transform minposFlag;
-    [SerializeField]
-    private GameObject CanvasPause;
-    [SerializeField]
-    private Transform maxposFlag;
-    private static float treeBottom;
-    [SerializeField]
-    GameObject[] hills;
-    [SerializeField]
-    GameObject[] trees;
-  
-   
-    [SerializeField]
-    GameObject finalFlag;
-
-    Camera cameraMain;
-
-
-    [SerializeField]
-    Rigidbody2D rigid;
-    float speed = 3f;
-    bool isJumping = false;
-
+    float jumpDist = 5f;
+    Animator anim;
+    private SpriteRenderer rend;
+    private Rigidbody2D rigidPlayer;
+    private bool isFlipped=false;
+    private bool isWalking = false;
+    private bool isJumping = false;
+    private float move;
     // Use this for initialization
     void Start () {
-        hillBottom = hillPos.position.y;
-        treeBottom = treePos.position.y;
-        CreateLevel(true);
-    }
+        rigidPlayer = GetComponent<Rigidbody2D>();
+        rend = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        Move();
-        GetInput();
-	}
-
-    private void GetInput()
-    {
-        if(Input.GetKeyDown("escape")&&!CanvasPause.activeSelf)
-        {
-            CanvasPause.SetActive(true);
-            Time.timeScale = 0f;
-        }
-    }
-
-    void Move()
-    {
-        //Only jump and acceleration and decelleration forward are allowed in this game
-        if (Input.GetKeyDown("space")&&!isJumping)
+       if(Input.GetButtonDown("Jump")&&!isJumping)
         {
             isJumping = true;
-            rigid.AddForce(new Vector2(5f, 5f), ForceMode2D.Impulse);
+            rigidPlayer.AddForce(new Vector2(move, jumpDist), ForceMode2D.Impulse);
         }
+    }
 
+    private void FixedUpdate()
+    {
+
+        move = Input.GetAxis("Horizontal");
         if (!isJumping)
         {
-            if (Input.GetAxis("Horizontal") > 0)
-                rigid.velocity = Vector2.right * speed;
-            if (Input.GetAxis("Horizontal") < 0)
-                rigid.velocity = Vector2.left * speed;
-        }
-    }
-
-
-    void CreateLevel(bool firstLevel)
-    {
-        ChunkManager.DeleteObjects();
-        CreateBackground();
-
-        if (!firstLevel)
-        {
-            PlaceFinalFlagPos();
-            ChunkManager.buildChunk();
-        }
-    }
-
-    
-
-    private void PlaceFinalFlagPos()
-    {
-        float yPos = UnityEngine.Random.Range(minposFlag.position.y, maxposFlag.position.y);
-        finalFlag.transform.Translate(new Vector3(0, yPos));
-
-    }
-
-    void CreateBackground()
-    {
+            rigidPlayer.velocity = new Vector2(move * speedMove, rigidPlayer.velocity.y);
         
-        float backgroundRandomValue = Mathf.Round(UnityEngine.Random.Range(1f, 3f));
-        if (backgroundRandomValue == 1f)
-        {
-            
-            //Background with only hills
-            for (int i=0; i<trees.Length;i++)
+            if (move < 0)
             {
-                trees[i].SetActive(false);
-            }
-            for (int i = 0; i < hills.Length; i++)
-            {
-                hills[i].SetActive(true);
-                hills[i].transform.position =  new Vector3(UnityEngine.Random.Range(minpos.position.x, maxpos.position.x), hillBottom);
-            }
-        }
-        else
-        {
-            if (backgroundRandomValue == 2f)
-            {
-                
-                //Background with only trees
-                for (int i= 0; i < trees.Length; i++)
-                {
-                    trees[i].SetActive(true);
-                    trees[i].transform.position = new Vector3(UnityEngine.Random.Range(minpos.position.x, maxpos.position.x), treeBottom);
-                }
-                for (int i = 0; i < hills.Length; i++)
-                {
-                    hills[i].SetActive(false);
-                }
+                isFlipped = true;
+                anim.SetBool("walk", true);
             }
             else
             {
-                if (backgroundRandomValue == 3f)
+
+                if (move > 0)
                 {
-                  
-                    //Background with trees and collines
-                    for(int i= 0; i < trees.Length; i++)
-                    {
-                        trees[i].SetActive(true);
-                        trees[i].transform.position = new Vector3(UnityEngine.Random.Range(minpos.position.x, maxpos.position.x), treeBottom);
-                    }
-                    for(int i = 0; i < hills.Length; i++)
-                    {
-                        hills[i].SetActive(true);
-                        hills[i].transform.position = new Vector3(UnityEngine.Random.Range(minpos.position.x, maxpos.position.x), hillBottom);
-                    }
+                    isFlipped = false;
+                    anim.SetBool("walk", true);
+                }
+                else
+                {
+                    anim.SetBool("walk", false);
                 }
             }
+
+            Flip();
         }
+
+
     }
+
+    void Flip()
+    {
+        if (!isFlipped)
+
+            rend.flipX = false;
+        else
+            rend.flipX = true;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag.Equals("floor"))
+        if(collision.gameObject.tag.Equals("floor"))
+        {
             isJumping = false;
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-        
-        if (collider.tag.Equals("limitLeft"))
-        {
-            
-            transform.Translate(new Vector3(2f, 0f));
-            
-        }
-        else
-        {
-            if (collider.tag.Equals("limitRight"))
-            {
-                rigid.gravityScale = 0f;
-                rigid.velocity = Vector2.zero;
-                transform.Translate(new Vector3(-25f, 0f));
-                CreateLevel(false);
-                rigid.gravityScale = 1;
-            }
-            else
-            {
-                if (collider.tag.Equals("gameOver"))
-                {
-                    SceneManager.LoadScene("GameOver");
-                }
-            }
-        }
-    }
 }
